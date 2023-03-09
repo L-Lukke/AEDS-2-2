d274468d76523ef2b4e4c39943853b59e144e0f7
Andreixzc <andrei.massaini@hotmail.com>
2023-03-09 01:00:12 -0300

    f

---
+++ Q01/TP02Q01.java
@@ -0,0 +1,18 @@
+public class TP02Q01{
+    public static boolean isFim(String word) {
+		if (word.equals("FIM"))
+			return false;
+		else 
+			return true;
+	}
+    public static void main(String [] args) throws Exception{
+        Personagem token = new Personagem();
+        String path;
+        path = MyIO.readLine();
+        while(!(path.contains("FIM"))){
+            token.ler(path);
+            token.imprimir();
+            path = MyIO.readLine();
+        }
+    }
+}
